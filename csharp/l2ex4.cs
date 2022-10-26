0d82b2a404f1223a81e42602c01392331f44d83a
Rômulo da Silva <roomulo.cavalkant@outlook.com>
2022-10-26 13:20:16 +0000

    started c# exercises

---
+++ csharp/l2ex4.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Program {
+    public static void Main(string[] args) {
+
+        Console.WriteLine("Digite a base e a altura do retâgulo:");
+        double base = Console.ReadLine();
+        double altura = Console.ReadLine();
+
+        
+
+    }
+}
