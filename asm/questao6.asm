d73278b23a0709d3f74efca993f84e0f8bcceba0
Rômulo da Silva <roomulo.cavalkant@outlook.com>
2022-11-07 20:27:44 +0000

    almost implementing on asm questao6

--- asm/questao6.asm
+++ asm/questao6.asm
@@ -1,22 +1,36 @@
 .text
-	main:
 	# Solu��o: divide n por 100, imprime o resultado. Divide por 10, imprime, divide por 1, imprime. Vual�.
-	
-	# L� inteiro & copia para t0
+
+	# coloca o valor 100 em $t0
+	addi $t0 $0 100
+	# Lê inteiro
 	addi $2 $0 5
 	syscall
-	add $t0 $0 $2
-	# Coloca zero a ser impresso
-	add $4 $0 $0
-	# Imprime zero
+	# Divide $2 (resultado da leitura) por 100 ($t0)
+	div $2 $t0
+	# Coloca o resultado da divisao em $4 para em seguida imprimir
+	mflo $4
 	addi $2 $0 1
 	syscall
+
+	# Adiciona o valor 10 em t0
+	addi $t0 $0 10
+	# Move o resto da divisao passada pra t1 e divide por 10
+	mfhi $t1
+	div $t1 $t0
+	# Move o resultado pra $4 e imprime
+	mflo $4
+	addi $2 $0 1
 	syscall
-	# Coloca inteiro em $2
+
+	# Move o resto da divisao passada pra $4 e imprime
 	add $4 $0 $t0
-	#imprime inteiro
 	addi $2 $0 1
 	syscall
 	
+
+	# Termina o programa
+	addi $2 $0 10
+	syscall
 	
 	
