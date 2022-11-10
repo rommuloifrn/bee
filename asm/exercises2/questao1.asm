4edc098faef9f5923719206a87326f318b035f73
rommuloifrn <roomulo.cavalkant@outlook.com>
2022-11-10 11:51:50 -0300

    Fixing l2e1

--- asm/exercises2/questao1.asm
+++ asm/exercises2/questao1.asm
@@ -15,11 +15,27 @@
     sub $t7 $t0 $t1
     # Shifts t7 31 bits to right (leaving only the signal bit)
     srl $t7 $t7 31
-    # Stores AND between t7 and 1 in a0 ($4) and prints it
+    # Stores AND between t7 and 1 in a0 ($4) and sets opcode to 1
     andi $a0 $t7 1
+    addi $v0 $0 1
+
+    # PROBLEMA DE PULO AQUI !!!!!!!!
+    # If result = 0 print t0, else print t1
+    beq $a0 0 maior
+    j menor
+    maior:
+    add $a0 $0 $t0
+    addi $v0 $0 1
+    syscall
+    menor:
+    add $a0 $0 $t1
     addi $v0 $0 1
     syscall
 
-    # Finishes the program
+
+    # New line & finishes the program
+    addi $4 $0 10
+    addi $2 $0 11
+    syscall
     addi $v0 $0 10
     syscall
