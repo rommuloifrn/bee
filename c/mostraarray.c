3e5685862ec990be649bab932b339e0eab2ef4da
rommuloifrn <roomulo.cavalkant@outlook.com>
2022-11-17 11:56:05 -0300

    errors in c

---
+++ c/mostraarray.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int x;
+    int myarr[10] = {3, 2, 6, 25};
+    scanf("%d", &x);
+    printf("%d\n", myarr[x]);
+
+    return 0;
+}
---
+++ c/random.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int x, y, i;
+    int myarr[] = {2, 26, 6, 25};
+    
+
+    printf("%lu", sizeof(myarr));
+
+
+    return 0;
+}
