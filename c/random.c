fd8192985f9001818a2ce7b67b9157569b680ad4
rommuloifrn <roomulo.cavalkant@outlook.com>
2022-11-17 18:31:37 -0300

    killing git video lessons

--- c/random.c
+++ c/random.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
 
 int main(){
-    int x, y, i;
-    int myarr[] = {2, 26, 6, 25};
-    
-
-    printf("%lu", sizeof(myarr));
-
-
-    return 0;
+    printf("%c\n", 65);
 }
---
+++ c/rgbaex.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    unsigned int rgb;
+    unsigned char r, g, b, alfa;
+    scanf("%u", &rgb);
+    printf("RGB = %08X\n", rgb);
+
+    /* Realizar conversões para as variáveis r, g, b e alfa */
+    r
+    return 0;
+}
